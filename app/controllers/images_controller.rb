c58c6823c642364940c1e2a2784ac3091013e921
Balraj Shah <BalrajShah@live.com>
2015-12-08 18:32:26 -0500

    fixed previous pull problems and working on navigation with arrow keys

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
 
   def index
     if params[:tag]
-      @images = Image.tagged(params[:tag])
+      @images = Image.tagged(params[:tag]).paginate(:page => params[:page], :per_page => 10)
     else
       @images = Image.all.paginate(:page => params[:page], :per_page => 1)
     end
