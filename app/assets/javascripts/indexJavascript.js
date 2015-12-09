e2f20e63acf685683fd6a36a58a38537912c89b5
Balraj Shah <BalrajShah@live.com>
2015-12-08 21:41:32 -0500

    Fixed so you can't go above total pages or below

--- app/assets/javascripts/indexJavascript.js
+++ app/assets/javascripts/indexJavascript.js
@@ -1,28 +1,17 @@
 $(document).ready(function(){
+  $('.pagination').hide();
 
-    $(document).bind('keyup', function(e){
-      if(e.which==39) {
-        var url = window.location.href;
-        var value = url.substring(url.lastIndexOf('/') + 1);
-        if(value.indexOf('images?page=') >= 0){
-          value = value.substring(value.lastIndexOf('=') + 1);
-          value = parseInt(value) + 1;
-        }else{
-          value = 1;
-        }
-        window.location = "images?page="+value;
-        debugger;
-      };
-      if(e.which==37){
+  $(document).bind('keyup', function(e){
+    if(e.which==37){
         var url = window.location.href;
         var value = url.substring(url.lastIndexOf('/') + 1);
         if(value.indexOf('images?page=') >= 0){
           value = value.substring(value.lastIndexOf('=') + 1);
           value = parseInt(value) - 1;
-        }else{
-          value = 1;
         }
+        if(value > 0){
         window.location = "images?page="+value;
+      };
       };
     });
 });
