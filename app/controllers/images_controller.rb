9626f0cb9f06699722f0164639d7b86be3064374
Arka Ganguli <arkaganguli1@gmail.com>
2015-12-08 23:32:15 -0500

    try taking tags to a different page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,11 +3,15 @@ class ImagesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    if params[:tag]
-      @images = Image.tagged(params[:tag]).paginate(:page => params[:page], :per_page => 10)
-    else
-      @images = Image.all.paginate(:page => params[:page], :per_page => 1)
-    end
+    # if params[:tag]
+    #   @images = Image.tagged(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    # else
+    @images = Image.all.paginate(:page => params[:page], :per_page => 1)
+    # end
+  end
+
+  def tags
+    @images = Image.tagged(params[:tag]).paginate(:page => params[:page], :per_page => 5)
   end
 
   def browse_tags
