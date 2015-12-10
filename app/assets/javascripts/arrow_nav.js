e0cea9094936ebb0ae4f458e28a25b4053a19fcf
Balraj Shah <BalrajShah@live.com>
2015-12-10 04:02:53 -0500

    cleaned up some of the js

--- app/assets/javascripts/arrow_nav.js
+++ app/assets/javascripts/arrow_nav.js
@@ -1,10 +1,11 @@
 var total_num_pages = 1;
+$(".pagination").hide();
 
 $(document).bind('keyup', function(e){
-  var cyclePage = true;
-  if(e.which==39 || e.which==37) {
-    var url = window.location.href;
-    var value = url.substring(url.lastIndexOf('/') + 1);
+
+  var cyclePage = !(window.location.href.indexOf("/images/") >= 0);
+  if((e.which==39 || e.which==37) && cyclePage) {
+    var value = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
     if(value.indexOf('images?page=') >= 0){
       if(e.which==37){
         value = parseInt(value.substring(value.lastIndexOf('=') + 1))-1;
@@ -16,10 +17,7 @@ $(document).bind('keyup', function(e){
     else{
       value = 2;
     }
-    if(window.location.href.indexOf("/images/") >= 0){
-      cyclePage = false;
-    }
-    if(value <= total_num_pages && value > 0 && (window.location.href.indexOf('/images') >= 0) && cyclePage){
+    if(value <= total_num_pages && value > 0 && (window.location.href.indexOf('/images') >= 0)){
       location.href = "images?page="+value;
     }
   }
