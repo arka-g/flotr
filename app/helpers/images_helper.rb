3cec6d6524f531e8a37cb4b40f11d547418482e6
Arka Ganguli <arkaganguli1@gmail.com>
2015-12-12 00:31:15 -0500

    fix auth issues

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,5 @@
 module ImagesHelper
   def tag_links(tags)
-    tags.split(',').map{|tag| link_to tag.strip, tag_path(tag.strip)}.join(', ')
+    tags.split(',').map{|tag| link_to tag.strip, tag_pictures_path(tag.strip)}.join(', ')
   end
 end
