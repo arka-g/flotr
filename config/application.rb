79889f29f25040f835dfaafa8d49b940c0d1d6f3
Rakesh Mistry <mistryrn@mcmaster.ca>
2015-12-11 19:59:19 -0500

    Styling for the Index/Main page is now complete.
    Added a new gem, "normalize-rails", which just links to a CSS reset.
    
    Still need to link the "Like" function to the db and figure out a
    way to differentiate the "show" page when users click Comment/Buy.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Flotr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
+
